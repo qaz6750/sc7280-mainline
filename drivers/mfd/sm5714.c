583c21402cd276108885c960a589f2b41fd67c8c
XiaoYeZi <qaz6750@outlook.com>
2026-08-03 17:19:12 +0800

    mfd: sm5714: Add shared interrupt handling
    SM5714 exposes charger, MUIC and fuel-gauge interrupt sources through
    one parent interrupt. Without a parent handler the child drivers cannot
    observe charger state transitions reliably.
    
    Initialize and acknowledge the interrupt sources in the MFD core,
    dispatch charger events through a notifier chain and only register child
    devices whose regmaps are available.
    
    Signed-off-by: XiaoYeZi <qaz6750@outlook.com>

--- drivers/mfd/sm5714.c
+++ drivers/mfd/sm5714.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <linux/i2c.h>
+#include <linux/interrupt.h>
 #include <linux/mfd/core.h>
 #include <linux/mfd/sm5714.h>
 #include <linux/module.h>
@@ -36,6 +37,115 @@ static const struct regmap_config sm5714_fuel_gauge_regmap_config = {
 	.max_register = 0x91,
 };
 
+int sm5714_register_notifier(struct sm5714 *sm5714,
+			     struct notifier_block *notifier)
+{
+	return blocking_notifier_chain_register(&sm5714->notifier, notifier);
+}
+EXPORT_SYMBOL_GPL(sm5714_register_notifier);
+
+int sm5714_unregister_notifier(struct sm5714 *sm5714,
+			       struct notifier_block *notifier)
+{
+	return blocking_notifier_chain_unregister(&sm5714->notifier, notifier);
+}
+EXPORT_SYMBOL_GPL(sm5714_unregister_notifier);
+
+static irqreturn_t sm5714_irq(int irq, void *data)
+{
+	struct sm5714 *sm5714 = data;
+	u8 interrupts[5];
+	unsigned int source;
+	unsigned int value;
+	int ret;
+
+	ret = regmap_read(sm5714->charger_regmap,
+			  SM5714_CHG_REG_INT_SOURCE, &source);
+	if (ret)
+		return IRQ_NONE;
+
+	if (source & SM5714_IRQ_SOURCE_CHARGER) {
+		ret = regmap_bulk_read(sm5714->charger_regmap,
+				       SM5714_CHG_REG_INT1, interrupts,
+				       ARRAY_SIZE(interrupts));
+		if (ret)
+			dev_err_ratelimited(sm5714->dev,
+					    "failed to read charger interrupts: %d\n",
+					    ret);
+		else
+			blocking_notifier_call_chain(&sm5714->notifier,
+						     SM5714_EVENT_CHARGER,
+						     interrupts);
+	}
+
+	if ((source & SM5714_IRQ_SOURCE_MUIC) && sm5714->muic_regmap) {
+		ret = regmap_bulk_read(sm5714->muic_regmap, 0x01,
+				       interrupts, 2);
+		if (ret)
+			dev_err_ratelimited(sm5714->dev,
+					    "failed to read MUIC interrupts: %d\n",
+					    ret);
+	}
+
+	if ((source & SM5714_IRQ_SOURCE_FUEL_GAUGE) &&
+	    sm5714->fuel_gauge_regmap) {
+		ret = regmap_read(sm5714->fuel_gauge_regmap,
+				  SM5714_FG_REG_INT, &value);
+		if (ret)
+			dev_err_ratelimited(sm5714->dev,
+					    "failed to read fuel-gauge interrupt: %d\n",
+					    ret);
+	}
+
+	return IRQ_HANDLED;
+}
+
+static int sm5714_irq_init(struct sm5714 *sm5714)
+{
+	u8 interrupts[5];
+	u8 charger_masks[5] = { 0xff, 0xff, 0xff, 0xff, 0xff };
+	u8 muic_masks[2] = { 0xff, 0xff };
+	unsigned int value;
+	int ret;
+
+	ret = regmap_bulk_write(sm5714->charger_regmap,
+				SM5714_CHG_REG_INTMSK1, charger_masks,
+				ARRAY_SIZE(charger_masks));
+	if (ret)
+		return ret;
+
+	ret = regmap_bulk_read(sm5714->charger_regmap, SM5714_CHG_REG_INT1,
+			       interrupts, ARRAY_SIZE(interrupts));
+	if (ret)
+		return ret;
+
+	if (sm5714->muic_regmap) {
+		ret = regmap_bulk_write(sm5714->muic_regmap, 0x03,
+					muic_masks, ARRAY_SIZE(muic_masks));
+		if (ret)
+			return ret;
+
+		ret = regmap_bulk_read(sm5714->muic_regmap, 0x01,
+				       interrupts, ARRAY_SIZE(muic_masks));
+		if (ret)
+			return ret;
+	}
+
+	if (sm5714->fuel_gauge_regmap) {
+		ret = regmap_write(sm5714->fuel_gauge_regmap,
+				   SM5714_FG_REG_INT_MASK, 0x000f);
+		if (ret)
+			return ret;
+
+		ret = regmap_read(sm5714->fuel_gauge_regmap,
+				  SM5714_FG_REG_INT, &value);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
 static int sm5714_probe(struct i2c_client *client)
 {
 	struct device *dev = &client->dev;
@@ -51,6 +161,7 @@ static int sm5714_probe(struct i2c_client *client)
 	sm5714->dev = dev;
 	sm5714->charger = client;
 	i2c_set_clientdata(client, sm5714);
+	BLOCKING_INIT_NOTIFIER_HEAD(&sm5714->notifier);
 
 	sm5714->charger_regmap = devm_regmap_init_i2c(client,
 						      &sm5714_charger_regmap_config);
@@ -68,11 +179,6 @@ static int sm5714_probe(struct i2c_client *client)
 				     "unexpected charger device ID %#x\n", device_id);
 	revision = device_id >> 3;
 
-	ret = devm_mfd_add_devices(dev, PLATFORM_DEVID_AUTO,
-				   &sm5714_charger_cell, 1, NULL, 0, NULL);
-	if (ret)
-		return dev_err_probe(dev, ret, "failed to add charger device\n");
-
 	sm5714->muic = devm_i2c_new_dummy_device(dev, client->adapter,
 						  SM5714_I2C_ADDR_MUIC);
 	if (IS_ERR(sm5714->muic))
@@ -84,6 +190,7 @@ static int sm5714_probe(struct i2c_client *client)
 		if (IS_ERR(sm5714->muic_regmap)) {
 			dev_warn(dev, "failed to initialize MUIC regmap: %pe\n",
 				 sm5714->muic_regmap);
+			sm5714->muic_regmap = NULL;
 		} else {
 			ret = regmap_read(sm5714->muic_regmap,
 					  SM5714_MUIC_REG_DEVICE_ID, &device_id);
@@ -107,6 +214,7 @@ static int sm5714_probe(struct i2c_client *client)
 	if (IS_ERR(sm5714->fuel_gauge_regmap)) {
 		dev_warn(dev, "failed to initialize fuel-gauge regmap: %pe\n",
 			 sm5714->fuel_gauge_regmap);
+		sm5714->fuel_gauge_regmap = NULL;
 		goto done;
 	}
 
@@ -114,15 +222,37 @@ static int sm5714_probe(struct i2c_client *client)
 			  &device_id);
 	if (ret) {
 		dev_warn(dev, "failed to read fuel-gauge device ID: %d\n", ret);
+		sm5714->fuel_gauge_regmap = NULL;
 		goto done;
 	}
 
-	ret = devm_mfd_add_devices(dev, PLATFORM_DEVID_AUTO,
-				   &sm5714_fuel_gauge_cell, 1, NULL, 0, NULL);
-	if (ret)
-		dev_warn(dev, "failed to add fuel-gauge device: %d\n", ret);
-
 done:
+	ret = sm5714_irq_init(sm5714);
+	if (ret)
+		return dev_err_probe(dev, ret, "failed to initialize interrupts\n");
+
+	ret = devm_mfd_add_devices(dev, PLATFORM_DEVID_AUTO,
+				   &sm5714_charger_cell, 1, NULL, 0, NULL);
+	if (ret)
+		return dev_err_probe(dev, ret, "failed to add charger device\n");
+
+	if (sm5714->fuel_gauge_regmap) {
+		ret = devm_mfd_add_devices(dev, PLATFORM_DEVID_AUTO,
+					   &sm5714_fuel_gauge_cell, 1,
+					   NULL, 0, NULL);
+		if (ret)
+			dev_warn(dev, "failed to add fuel-gauge device: %d\n",
+				 ret);
+	}
+
+	if (client->irq > 0) {
+		ret = devm_request_threaded_irq(dev, client->irq, NULL, sm5714_irq,
+						IRQF_ONESHOT, dev_name(dev), sm5714);
+		if (ret)
+			return dev_err_probe(dev, ret,
+					     "failed to request interrupt\n");
+	}
+
 	if (device_property_read_bool(dev, "wakeup-source")) {
 		ret = devm_device_init_wakeup(dev);
 		if (ret)
--- include/linux/mfd/sm5714.h
+++ include/linux/mfd/sm5714.h
@@ -4,6 +4,7 @@
 #define __LINUX_MFD_SM5714_H
 
 #include <linux/bits.h>
+#include <linux/notifier.h>
 #include <linux/types.h>
 
 struct device;
@@ -18,6 +19,11 @@ struct regmap;
 #define SM5714_MUIC_REG_DEVICE_ID	0x00
 #define SM5714_FG_REG_DEVICE_ID		0x00
 
+#define SM5714_CHG_REG_INT_SOURCE	0x00
+#define SM5714_CHG_REG_INT1		0x01
+#define SM5714_CHG_REG_INT2		0x02
+#define SM5714_CHG_REG_INTMSK1		0x07
+#define SM5714_CHG_REG_INTMSK2		0x08
 #define SM5714_CHG_REG_STATUS1		0x0d
 #define SM5714_CHG_REG_STATUS2		0x0e
 #define SM5714_CHG_REG_CNTL2		0x14
@@ -31,6 +37,11 @@ struct regmap;
 #define SM5714_CHG_STATUS2_CHGON		BIT(3)
 #define SM5714_CHG_STATUS2_TOPOFF	BIT(5)
 
+#define SM5714_CHG_INT1_VBUSPOK		BIT(0)
+#define SM5714_CHG_INT2_NOBAT		BIT(2)
+#define SM5714_CHG_INT2_CHGON		BIT(3)
+#define SM5714_CHG_INT2_TOPOFF		BIT(5)
+
 #define SM5714_CHG_VBUS_LIMIT_MASK	GENMASK(6, 0)
 #define SM5714_CHG_BATREG_MASK		GENMASK(5, 0)
 #define SM5714_CHG_OP_MODE_MASK		GENMASK(3, 0)
@@ -43,6 +54,8 @@ struct regmap;
 #define SM5714_CHG_OTG_CURRENT_900MA	BIT(6)
 
 #define SM5714_FG_REG_SYSTEM_STATUS	0x10
+#define SM5714_FG_REG_INT		0x02
+#define SM5714_FG_REG_INT_MASK		0x04
 #define SM5714_FG_REG_SRAM_RADDR		0x8c
 #define SM5714_FG_REG_SRAM_RDATA		0x8d
 
@@ -54,6 +67,14 @@ struct regmap;
 #define SM5714_FG_SRAM_VBAT_AVG		0x08
 #define SM5714_FG_SRAM_CURRENT_AVG	0x09
 
+#define SM5714_IRQ_SOURCE_MUIC		BIT(0)
+#define SM5714_IRQ_SOURCE_CHARGER	BIT(1)
+#define SM5714_IRQ_SOURCE_FUEL_GAUGE	BIT(2)
+
+enum sm5714_event {
+	SM5714_EVENT_CHARGER,
+};
+
 struct sm5714 {
 	struct device *dev;
 	struct i2c_client *charger;
@@ -62,6 +83,12 @@ struct sm5714 {
 	struct regmap *charger_regmap;
 	struct regmap *muic_regmap;
 	struct regmap *fuel_gauge_regmap;
+	struct blocking_notifier_head notifier;
 };
+
+int sm5714_register_notifier(struct sm5714 *sm5714,
+			     struct notifier_block *notifier);
+int sm5714_unregister_notifier(struct sm5714 *sm5714,
+			       struct notifier_block *notifier);
 
 #endif
